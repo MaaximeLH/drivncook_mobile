1b060a30355e5eaf8812b6e1c104b30247a3bd27
MaaximeLH <lehenaff.maxime@yahoo.Fr>
2020-07-08 14:36:45 +0200

    Clean-up des dépendances

--- app/src/main/java/com/example/drivncook/MainActivity.java
+++ app/src/main/java/com/example/drivncook/MainActivity.java
@@ -10,13 +10,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.VolleyLog;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
--- app/src/main/java/com/example/drivncook/UserActivity.java
+++ app/src/main/java/com/example/drivncook/UserActivity.java
@@ -4,9 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class UserActivity extends AppCompatActivity {
 
